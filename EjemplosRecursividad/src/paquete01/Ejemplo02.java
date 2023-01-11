62842e62f13bd2ac72fb19a4b9c255172824682d
santiagoCH14 <santych.4.2@gmail.com>
2023-01-11 09:59:06 -0500

    Programacion

--- EjemplosRecursividad/src/paquete01/Ejemplo02.java
+++ EjemplosRecursividad/src/paquete01/Ejemplo02.java
@@ -8,16 +8,16 @@ package paquete01;
 public class Ejemplo02 {
 
     public static void main(String[] args) {
-        int numero = 7;
+        int numero = 20;
         System.out.printf("Números del %d al 1\n", numero);
         imprimirNumeros(numero);
     }
 
     public static void imprimirNumeros(int numero) {
-        
+
         System.out.printf("El número es: %d\n", numero);
         if (numero > 1) {  // se busca una condición que se 
-                           // acerque al caso base
+            // acerque al caso base
             numero = numero - 1;
             imprimirNumeros(numero);
         }
--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -10,12 +10,28 @@ public class Ejemplo06 {
 
         int[] valor1 = {100, 1000, 200, 2000, 3000};
         int[] valor2 = {1, 2, 3, 4, 5};
-        
+
         // Encontrar la suma de las posiciones
         // [0][0]
         // [1][1]
         // hasta el final
+        for (int i = 0; i < valor1.length; i++) {
+            //System.out.printf("El valor es: %d\n",
+            //obtenerSuma(valor1[i], valor2[i]));
 
+        }
+        for (int i = 0; i < valor1.length; i++) {
+            //int num1 = valor1[i];
+            //int num2 = valor2[i];
+            //System.out.printf("El valor es: %d\n",
+            //btenerSuma(num1, num2));
+        }
+        for (int i = 0; i < valor1.length; i++) {
+            int num1 = valor1[i];
+            int num2 = valor2[i];
+            int suma = obtenerSuma(num1, num2);
+            System.out.printf("El valor es: %d\n");
+        }
     }
 
     public static int obtenerSuma(int a, int b) {
